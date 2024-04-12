139545c60a4592c17db9d695607aa3525dde5c9b
sachihiroo <saif3antar@gmail.com>
2024-04-12 14:22:01 +0100

    script

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
 
+
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """display a list of all states and cities"""
