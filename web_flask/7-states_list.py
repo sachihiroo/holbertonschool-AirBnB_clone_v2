e852bd0aec6a69c46b35fb45ccaa64e796abf367
sachihiroo <saif3antar@gmail.com>
2024-04-12 15:25:39 +0100

    script

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,26 +2,24 @@
 """Create a Flask application instance"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """display a list of all states and cities"""
-    stateslist = sorted(storage.all("State").values(),
-                        key=lambda item: item.name)
-
-    for state in stateslist:
-        state.cities = sorted(state.cities, key=lambda item: item.name)
-    return render_template("8-cities_by_states.html", stateslist=stateslist)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Returns the list of all States in the database."""
+    states = sorted(storage.all("State").values(), key=lambda s: s.name)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def remove_sess(self):
-    """removing the current SQLAlchemy Session after each request"""
+def close(self):
+    """Closes the database session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    """Run the app in debug mode"""
+    # Run the Flask application
     app.run(host="0.0.0.0", port=5000)
