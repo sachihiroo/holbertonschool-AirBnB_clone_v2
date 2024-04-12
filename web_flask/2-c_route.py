cb40e855c21c96d80c957d37cf0a723c809498e8
sachihiroo <saif3antar@gmail.com>
2024-04-12 10:26:11 +0100

    script

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Create a Flask application instance"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Return Hello HBNB! message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """return HBNB .* message"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def HBNB(text):
+    """return text .* message"""
+    text = text.replace("_", " ")
+    return "C {text}"
+
+
+if __name__ == "__main__":
+    """Run the app in debug mode"""
+    app.run(host="0.0.0.0", port=5000)
