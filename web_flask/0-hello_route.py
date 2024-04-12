12b38663095b0e3262ac8f266e5e2e8de0b2ce2c
sachihiroo <saif3antar@gmail.com>
2024-04-12 09:50:30 +0100

    script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 from flask import Flask
+
 # Create a Flask application instance
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     """Return Hello HBNB! message"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     # Run the app in debug mode
